23715810fbb293d55044337cfc03e9e7145f66fc
Nikola Stankovic <jonnywintech@gmail.com>
2026-01-15 10:39:29 +0100

    initial commit

---
+++ lib/expenses.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
