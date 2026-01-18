06170ea2ee5bb4589628cef1803aa3ac272b6f25
Nikola Stankovic <jonnywintech@gmail.com>
2026-01-18 19:31:51 +0100

    First input and label

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:expenses_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expenses_tracker/models/expense.dart';
+import 'package:expenses_tracker/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -29,7 +30,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const Text('Hello from bottom sheet!'),
+      builder: (ctx) => NewExpense(),
     );
   }
 
