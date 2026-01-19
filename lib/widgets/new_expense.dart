d26ef2c26beba493ebef068a4737b8e6a77cfcdb
Nikola <jonnywintech@gmail.com>
2026-01-19 22:13:52 +0100

    one way to print / save statement in memory

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  NewExpense({super.key});
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
             decoration: InputDecoration(
               labelText: 'Title',
               icon: Icon(Icons.title),
             ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_enteredTitle);
+                },
+                child: Text('Save Expense'),
+              ),
+            ],
           ),
         ],
       ),
