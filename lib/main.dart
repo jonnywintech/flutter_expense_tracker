c2ad558672f44212f3a0510bc167a2a12cae257b
Nikola Stankovic <jonnywintech@gmail.com>
2026-01-22 15:45:20 +0100

    initial theme configuring

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,21 @@ import 'package:expenses_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
+      theme: ThemeData().copyWith(
+        primaryColor: Color(Colors.blue.shade600.value),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue.shade600,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue.shade600,
+          ),
+        ),
+        scaffoldBackgroundColor: Colors.blue.shade100,
       ),
       home: Expenses(),
     ),
